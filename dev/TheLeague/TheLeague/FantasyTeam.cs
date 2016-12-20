c7bd9a2ea4faaa754f0353d0251a64dd22db1829
Jiho Chang <jihochang@gmail.com>
2016-12-19 17:12:22 -0800

    Get Teams, Players, Stats for League Mikey

---
+++ dev/TheLeague/TheLeague/FantasyTeam.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+
+namespace TheLeague
+{
+    public class FantasyTeam
+    {
+        public FantasyTeam()
+        {
+            Players = new List<FantasyPlayer>();
+        }
+
+        public int Id { get; set; }
+
+        public string Manager { get; set; }
+
+        public List<FantasyPlayer> Players { get; set; }
+    }
+
+    public class FantasyPlayer
+    {
+        public FantasyPlayer()
+        {
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+
+        public FantasyStats SeasonStats { get; set; }
+
+        //public FantasyStats Past30DayStats { get; set; }
+    }
+
+    public class FantasyStats
+    {
+        /*
+          [0] = Games Played
+          [3] = FG Attempted
+          [4] = FG Made
+          [5] = FG%
+          [6] = FT Attempted
+          [7] = FT Made
+          [8] = FT%
+          [10] = Threes Made
+          [15] = Rebounds 
+          [16] = Assists
+          [17] = Steals
+          [18] = Blocks
+          [19] = Turnover
+        */
+
+        public FantasyStats(YahooSports.Api.Sports.Models.Player.PlayerStats.PlayerStat[] stats)
+        {
+            GamePlayed = ConvertStatValue(stats[0].Value);
+            FGA = ConvertStatValue(stats[3].Value);
+            FGM = ConvertStatValue(stats[4].Value);
+            FTA = ConvertStatValue(stats[6].Value);
+            FTM = ConvertStatValue(stats[7].Value);
+            ThreesMade = ConvertStatValue(stats[10].Value);
+            Rebounds = ConvertStatValue(stats[15].Value);
+            Assists = ConvertStatValue(stats[16].Value);
+            Steals = ConvertStatValue(stats[17].Value);
+            Blocks = ConvertStatValue(stats[18].Value);
+            Turnover = ConvertStatValue(stats[19].Value);
+        }
+
+        private int ConvertStatValue(string value)
+        {
+            if(value == "-")
+            {
+                return 0;
+            }
+            else
+            {
+                return int.Parse(value);
+            }
+        }
+
+        public int GamePlayed { get; set; }
+
+        public int FGA { get; set; }
+
+        public int FGM { get; set; }
+
+        public int FTA { get; set; }
+
+        public int FTM { get; set; }
+
+        public int ThreesMade { get; set; }
+
+        public int Rebounds { get; set; }
+
+        public int Assists { get; set; }
+
+        public int Steals { get; set; }
+
+        public int Blocks { get; set; }
+
+        public int Turnover { get; set; }
+    }
+}
---
+++ dev/TheLeague/TheLeague/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using YahooSports.Api;
+using YahooSports.Api.Sports.Models;
+using YahooSports.OAuthLib.Core;
+using YahooSports.OAuthLib.Providers;
+
+namespace TheLeague
+{
+    class Program
+    {
+        // Yahoo Developer Network
+        // Registered Application "Basketball Research"
+        // ConsumerKey and ConsumerSecret
+        public const string ClientId = "dj0yJmk9VDFrMmpOck1zZ0ppJmQ9WVdrOVlWZG5NWGw1TldNbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD04Yw--";
+        public const string ClientSecret = "b3d41f9349b9b818d7a1af5f100ee9e267768c13";
+
+        private static readonly IsolatedStorageFile Storage = IsolatedStorageFile.GetUserStoreForAssembly();
+
+        private const string TOKEN_FILE = "token";
+        private static AccessToken Token;
+
+        private static SportsProvider Client;
+
+
+        public static void Main(string[] args)
+        {
+            SetupClient();
+
+            FantasyContent leagueContent = Client.ExecuteRequest<FantasyContent>("http://fantasysports.yahooapis.com/fantasy/v2/league/364.l.45575");
+            leagueContent = Client.ExecuteRequest<FantasyContent>("http://fantasysports.yahooapis.com/fantasy/v2/league/364.l.45575/settings");
+            int numTeams = int.Parse(leagueContent.League.NumTeams);
+
+            List<FantasyTeam> league = new List<FantasyTeam>();
+
+            //FantasyTeam jiho = GetTeam("45575", "2");
+
+            for(int i=1; i<=numTeams; i++)
+            {
+                league.Add(GetTeam("45575", i.ToString()));
+            }
+
+            // nba_id = 364
+            //FantasyContent content = Client.ExecuteRequest<FantasyContent>("http://fantasysports.yahooapis.com/fantasy/v2/game/nba");
+
+            //content = Client.ExecuteRequest<FantasyContent>("http://fantasysports.yahooapis.com/fantasy/v2/league/364.l.45575");
+
+            //content = Client.ExecuteRequest<FantasyContent>("http://fantasysports.yahooapis.com/fantasy/v2/team/364.l.45575.t.2/roster/players");
+
+            //content = Client.ExecuteRequest<FantasyContent>("http://fantasysports.yahooapis.com/fantasy/v2/player/364.p.4612/stats");
+        }
+
+        private static FantasyTeam GetTeam(string leagueId, string teamId)
+        {
+            FantasyTeam team = new FantasyTeam();
+
+            string teamUrl = "http://fantasysports.yahooapis.com/fantasy/v2/team/364.l." + leagueId + ".t." + teamId + "/roster/players";
+            FantasyContent teamContent = Client.ExecuteRequest<FantasyContent>(teamUrl);
+
+            team.Id = teamContent.Team.Id;
+            team.Manager = teamContent.Team.Managers[0].Nickname;
+
+            foreach (Team.Roster.TeamPlayer item in teamContent.Team.TeamRoster.Players)
+            {
+                FantasyPlayer player = new FantasyPlayer();
+                player.Id = item.Id;
+                player.Name = item.Name.Full;
+            
+                string playerUrl = "http://fantasysports.yahooapis.com/fantasy/v2/player/364.p." + player.Id + "/stats";
+                FantasyContent playerContent = Client.ExecuteRequest<FantasyContent>(playerUrl);
+            
+                player.SeasonStats = new FantasyStats(playerContent.Player.PlayerStatistics.Stats);
+                team.Players.Add(player);
+            }
+
+            return team;
+        }
+
+        private static void SetupClient()
+        {
+            GetToken(false);
+
+            Client = new SportsProvider("", ClientId, ClientSecret, Token);
+            Thread.Sleep(1000);
+
+            try
+            {
+                FantasyContent content = Client.ExecuteRequest<FantasyContent>("http://fantasysports.yahooapis.com/fantasy/v2/game/nba");
+            }
+            catch (Exception exception)
+            {
+                if(exception.HResult == -2146233088)
+                {
+                    GetToken(true);
+                    Client = new SportsProvider("", ClientId, ClientSecret, Token);
+                    Thread.Sleep(1000);
+                    return;
+                }
+
+                throw;
+            }
+        }
+
+        private static void GetToken(bool refreshToken)
+        {
+            if (Storage.FileExists(TOKEN_FILE))
+            {
+                try
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    using (Stream stream = Storage.OpenFile(TOKEN_FILE, FileMode.Open))
+                    {
+                        Token = formatter.Deserialize(stream) as AccessToken;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR: Error deserializing the token!");
+                }
+            }
+
+            if(Token == null || refreshToken)
+            {
+                string appUrl = string.Empty;
+                string authCallback = "oob";
+
+                YahooOAuthProvider provider = new YahooOAuthProvider(ClientId, ClientSecret, string.Empty, authCallback);
+
+                Func<string, string> userAuthCallback = url =>
+                {
+                    using (Process p = new Process())
+                    {
+                        p.StartInfo = new ProcessStartInfo("iexplore.exe", url);
+                        p.Start();
+                        return Microsoft.VisualBasic.Interaction.InputBox("Enter key from (" + url + "):");
+                    }
+                };
+
+                Token = provider.Authenticate(userAuthCallback);
+            }
+
+            using (Stream stream = Storage.OpenFile(TOKEN_FILE, FileMode.Create))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, Token);
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+}
